39fd0de7a3fb7f699817001340573e8d7f3feeb6
Thalles Alexsander <thallesander@hotmail.com>
2025-01-27 21:51:01 -0300

    adds the frontend code and connecting it to server backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(null);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
-        setData(response.data.message);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+  const searchMovies = async () => {
+    if(!query) {
+      alert('Por favor, digite um nome de filme!');
+      return;
+    }
+
+    try {
+      const response = await axios.get(`http://localhost:5000/api/movies?query=${encodeURIComponent(query)}`);
+      setMovies(response.data);
+    } catch (error) {
+      console.log('Erro:', error);
+    }
+  };
 
   return (
-    <div>
-      <header>
-        <h1>TESTE</h1>
-        <p>{data ? data : 'Loading...'}</p>
-      </header>
-    </div>
-  )
+    <div className="App">
+            <header className="App-header">
+                <h1>Busca de Filmes</h1>
+                <input
+                    type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Digite o nome do filme"
+                />
+                <button onClick={searchMovies}>Buscar</button>
+                <div className="movies">
+                    {movies.map(movie => (
+                        <div key={movie.id} className="movie">
+                            <h2>{movie.title}</h2>
+                            <img src={movie.poster_path} alt={movie.title} />
+                            <p><strong>Média de Votos:</strong> {movie.vote_average}</p>
+                            <p><strong>Descrição:</strong> {movie.overview}</p>
+                            <p><strong>Quantidade de Votos:</strong> {movie.vote_count}</p>
+                        </div>
+                    ))}
+                </div>
+            </header>
+        </div>
+  );
 }
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+const helmet = require('helmet');
 const app = express();
 const port = 5000;
 
-// Use CORS middleware
-app.use(cors());
+const apiKey = 'APIKEY' //substituir pela api-key
 
-// Dummy endpoint
-app.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
+// Use CORS middleware
+app.use(cors({
+    origin: 'http://localhost:5173', //porta do frontend
+}));
+
+app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          fontSrc: ["'self'", "https://fonts.gstatic.com"],
+          styleSrc: ["'self'", "https://fonts.googleapis.com"],
+          // Adicione outras diretivas conforme necessário
+        },
+      },
+    })
+);
+
+app.get('/api/movies', async (req, res) => {
+    const movieQuery = req.query.query;
+    if (!movieQuery) {
+        return res.status(400).json({ error: 'O parâmetro query é obrigatório' });
+    }
+
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(movieQuery)}`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        const movies = response.data.results.map(movie => ({
+            id: movie.id,
+            title: movie.title,
+            poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            vote_average: movie.vote_average,
+            overview: movie.overview,
+            vote_count: movie.vote_count
+        }));
+
+        const sortedMovies = movies.sort((a, b) => b.vote_average - a.vote_average);
+
+        res.json(sortedMovies);
+    } catch (error) {
+        console.error('Erro ao buscar os dados da API:', error);
+        res.status(500).json({ error: 'Erro ao buscar os dados da API' });
+    }
 });
 
 app.listen(port, () => {
